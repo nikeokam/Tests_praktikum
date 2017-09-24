f5920e5d5438824058288be9aa74a9c80f2daa20
Nikita Okamanchuk <nikitaokamanchuk@MacBook-Pro-Nikita.local>
2017-09-24 23:25:06 +0300

    Esimene praks

---
+++ Tests/WeatherForecastTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nikitaokamanchuk on 24.09.17.
+ */
+public class WeatherForecastTest {
+    @Test
+    }
+
+    @Test
+    public void getCurrentTemperature()  {
+        assertTrue(false);
+    }
+    @Test
+    public void getHighestAndLowest()  {
+        assertTrue(false);
+    }
+    @Test
+    public void getCoordinates()  {
+        assertTrue(false);
+    }
+}
---
+++ src/WeatherForecast.java
@@ -0,0 +1,13 @@
+/**
+ * Created by nikitaokamanchuk on 24.09.17.
+ */
+public class WeatherForecast {
+    private String name;
+
+    public WeatherForecast(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
